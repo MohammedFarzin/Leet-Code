4392a9cbea86857928112de9b5a174e472121332
MohammedFarzin <mohammedfarzin.m123@gmail.com>
2023-07-20 15:13:11 +0530

    83. Remove duplicate from sorted list

---
+++ 83. Remove duplicated from sorted list.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def delete_duplicated(self, head):
+        curr = head
+
+        while curr and curr.next:
+            if curr.next.val == curr.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+            
+        return head
+
